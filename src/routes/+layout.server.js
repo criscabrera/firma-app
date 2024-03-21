de76b50a748e3ca47c4f55441c6f988296fa60d8
Cristian Cabrera <cristian.cabrera.s@gmail.com>
2024-03-20 22:32:51 -0300

    it's working! don't know why much, but it works :)

--- src/routes/+layout.server.js
+++ src/routes/+layout.server.js
@@ -3,6 +3,6 @@
 
 export const load = async ({ locals: { getSession } }) => {
   return {
-    session: await getSession(),
+    session: await getSession()
   }
 }
--- src/routes/auth/callback/+server.js
+++ src/routes/auth/callback/+server.js
@@ -8,10 +8,19 @@ export const GET = async (event) => {
 	const code = url.searchParams.get('code'); //as string
 	const next = url.searchParams.get('next') ?? '/';
 
+
+  //new code
+  const user = await supabase.auth.getUser();
+	console.log('user', user.data.user);
+	const session = await supabase.auth.getSession();
+	console.log('session', session.data.session);
+	console.log(code);
+  //end of new code
+
   if (code) {
     const { error } = await supabase.auth.exchangeCodeForSession(code)
     if (!error) {
-      redirect(303, `/${next.slice(1)}`);
+      throw redirect(303, `/${next.slice(1)}`);
     }
   }
 
